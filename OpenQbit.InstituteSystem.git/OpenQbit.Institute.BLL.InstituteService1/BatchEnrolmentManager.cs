b350b87d6a6d55db9619c2e90133c2e339564e96
ChamikaAbeygunarathna <cpabeygunarathna@gmail.com>
2017-06-11 14:37:05 +0530

    create BLL

---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.InstituteService1/BatchEnrolmentManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using OpenQbit.Institute.DAL.DataAccess;
+using OpenQbit.Institute.Common.Models;
+
+namespace OpenQbit.Institute.BLL.InstituteService1
+{
+    class BatchEnrolmentManager
+    {
+        private Repository _db = new Repository();
+
+
+        public bool CreateBatchEnrolment(int TimeDuration, int BatchId, int StudentI)
+        {
+            BatchEnrolment newBatchEnrolment = new BatchEnrolment();
+            return _db.Create<BatchEnrolment>(newBatchEnrolment);
+        }
+
+        public bool RemoveBatchEnrolment(int BatchEnrolmentId)
+        {
+            BatchEnrolment newBatchEnrolment = new BatchEnrolment();
+            return _db.Delete<BatchEnrolment>(newBatchEnrolment);
+        }
+        
+        public BatchEnrolment FindBatchEnrolment(int BatchEnrolmentId)
+        {
+            BatchEnrolment newBatchEnrolment = new BatchEnrolment();
+            return _db.FindList<BatchEnrolment>(BatchEnrolmentId);
+        }
+    }
+}
---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.InstituteService1/BranchManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQbit.Institute.DAL.DataAccess;
+using OpenQbit.Institute.Common.Models;
+
+namespace OpenQbit.Institute.BLL.InstituteService1
+{
+    class BranchManager
+    {
+        private Repository _db = new Repository();
+
+
+        public bool CreateBranch(string BranchName, string Address, int TelNo, int InstituteId)
+        {
+            Branch newBranch = new Branch();
+            return _db.Create<Branch>(newBranch);
+        }
+
+        public bool RemoveBranch(int BranchID)
+        {
+            Branch newBranch = new Branch();
+            return _db.Delete<Branch>(newBranch);
+        }
+        public bool RemoveBranch(string BranchName)
+        {
+            Branch newBranch = new Branch();
+            return _db.Delete<Branch>(newBranch);
+        }
+        public Branch FindDocument(int BranchId)
+        {
+            Branch newBranch = new Branch();
+            return _db.FindList<Branch>(BranchId);
+        }
+    }
+}
