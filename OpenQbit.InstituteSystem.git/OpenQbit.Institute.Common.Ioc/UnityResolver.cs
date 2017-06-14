c3a6b0719efe7960e069f521fd30efddf450b8fe
ChamikaAbeygunarathna <cpabeygunarathna@gmail.com>
2017-06-14 10:28:58 +0530

    Checking step by step in IOC

---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.Common.Ioc/UnityResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Institute.DAL.DataAccess;
+using OpenQbit.Institute.DAL.DataAccess.Contracts;
+using OpenQbit.Institute.Common.Utils.Log;
+using OpenQbit.Institute.BLL.Service.Contracts;
+using OpenQbit.Institute.BLL.InstituteService1;
+
+using Microsoft.Practices.Unity;
+
+namespace OpenQbit.Institute.Common.Ioc
+{
+    public static class UnityResolver
+    {
+        private static readonly IUnityContainer Container = new UnityContainer();
+
+        private static void Register()
+        {
+            Container.RegisterType<IBranchResourceAllocationManager, BranchResourceAllocationManager>();
+            Container.RegisterType<IRepository, Repository>();
+            Container.RegisterType<ILogger, LoggerB>();
+        }
+
+        public static T Resolve<T>()
+        {
+            T defaultT = default(T);
+            var resolved = Container.Resolve<T>();
+            return (resolved == null) ? defaultT : resolved;
+        }
+
+        public static IUnityContainer GetContainer()
+        {
+            return Container;
+        }
+    }
+}
