cbac04ac63a32aad052581682fcca8d6bc16e541
ashanTharuka <ashantharuka371@outlook.com>
2017-06-18 00:04:18 +0530

    Test BLL and Test DAL Create

---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.Presentation.WebTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQbit.Institute.Presentation.WebTest.Startup))]
+namespace OpenQbit.Institute.Presentation.WebTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
