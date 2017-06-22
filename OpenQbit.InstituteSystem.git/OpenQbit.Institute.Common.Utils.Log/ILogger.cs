adc66bf952cd900d87590eba3e885aa15874aff7
ashanTharuka <ashantharuka371@outlook.com>
2017-06-22 15:06:17 +0530

    Add Util Project

---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.Common.Utils.Log/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Institute.Common.Utils.Log
+{
+    public interface ILogger
+    {
+        bool LogError(string err);
+    }
+}
