81c2bfd10d7c79e924753c30b41b51bffcd65752
ChamikaAbeygunarathna <cpabeygunarathna@gmail.com>
2017-06-30 13:47:14 +0530

    Create Basic Back-end to Implement UI

---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.Presentation.Web/Models/Student/StudentItems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpenQbit.Institute.Presentation.Web.Models.Student
+{
+    public class StudentItems
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Batch Name")]
+        public int StudentId { get; set; }
+
+        [Display(Name = "Name")]
+        public string StudentName { get; set; }
+
+        [Display(Name = "Address")]
+        public double Address { get; set; }
+
+        [Display(Name = "Email")]
+        public int Email { get; set; }
+
+        [Display(Name = "TelNo")]
+        public int TelNo { get; set; }
+
+        [Display(Name = "Parent Name")]
+        public string ParentInformationId { get; set; }
+
+        //[Display(Name = "TelNo")]
+        //public double TelNo { get; set; }
+    }
+}
---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.Presentation.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQbit.Institute.Presentation.Web.Startup))]
+namespace OpenQbit.Institute.Presentation.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
