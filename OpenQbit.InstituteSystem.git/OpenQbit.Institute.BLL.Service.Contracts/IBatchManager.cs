7d9d976eb8a68d21560cd7c96401e11696a9ad98
ChamikaAbeygunarathna <cpabeygunarathna@gmail.com>
2017-07-05 14:34:35 +0530

    Model changes

---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.Service.Contracts/IBatchManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Institute.Common.Models;
+
+namespace OpenQbit.Institute.BLL.Service1.Contracts
+{
+    public interface IBatchManager
+    {
+
+        bool Create(Batch batch);
+        Batch FindById(int batchId);
+        bool DeleteById(int batchId);
+
+        bool Delete(Batch batch);
+
+        Batch Find(Expression<Func<Batch, bool>> predicate);
+
+        List<Batch> FindList(Expression<Func<Batch, bool>> predicate);
+
+        bool Update(Batch batch);
+
+        List<Batch> GetAll();
+
+        bool Save();
+    }
+}
---
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.Service.Contracts/IBranchManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Institute.Common.Models;
+
+namespace OpenQbit.Institute.BLL.Service1.Contracts
+{
+    public interface IBranchManager
+    {
+
+
+        bool Create(Branch branch);
+        Branch FindById(int branchId);
+        bool DeleteById(int branchId);
+
+        bool Delete(Branch branch);
+
+        Branch Find(Expression<Func<Branch, bool>> predicate);
+
+        List<Branch> FindList(Expression<Func<Branch, bool>> predicate);
+
+        bool Update(Branch branch);
+
+        List<Branch> GetAll();
+
+        bool Save();
+    }
+}
