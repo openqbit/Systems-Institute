f2b7cd2a867a28333ed22830a4986fab719f4838
Sandun Prasanga <Sandun Prasanga@SANDUNPRASANGA>
2017-07-11 13:44:51 +0530

    IStudent Manager implemented

--- OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.Service.Contracts/IStudentManager.cs
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.Service.Contracts/IStudentManager.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
+using OpenQbit.Institute.Common.Models;
 
 namespace OpenQbit.Institute.BLL.Service.Contracts
 {
     interface IStudentManager
     {
+
+
+        bool Create(Student student);
+        Student FindById(int studentId);
+        bool DeleteById(int studentId);
+
+        bool Delete(Student student);
+
+        Student Find(Expression<Func<Student, bool>> predicate);
+
+        List<Student> FindList(Expression<Func<Student, bool>> predicate);
+
+        bool Update(Student student);
+
+        List<Student> GetAll();
+
+        bool Save();
+
     }
 }
