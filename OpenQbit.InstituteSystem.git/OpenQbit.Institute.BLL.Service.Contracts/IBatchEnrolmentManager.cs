91a7a3b0bfd51cca516ddacb8b821878338a2794
Sandun Prasanga <Sandun Prasanga@SANDUNPRASANGA>
2017-07-11 13:39:46 +0530

    IBatch Enrolment Implemented

--- OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.Service.Contracts/IBatchEnrolmentManager.cs
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.Service.Contracts/IBatchEnrolmentManager.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using OpenQbit.Institute.Common.Models;
+using System.Linq.Expressions;
 namespace OpenQbit.Institute.BLL.Service1.Contracts
 {
    public  interface IBatchEnrolmentManager
     {
+
+        bool Create(BatchEnrolment batchEnrolment);
+        BatchEnrolment FindById(int batchEnrolmentId);
+        bool DeleteById(int batchEnrolmentId);
+
+        bool Delete(BatchEnrolment batchEnrolment);
+
+        BatchEnrolment Find(Expression<Func<BatchEnrolment, bool>> predicate);
+
+        List<BatchEnrolment> FindList(Expression<Func<BatchEnrolment, bool>> predicate);
+
+        bool Update(BatchEnrolment batchEnrolment);
+
+        List<BatchEnrolment> GetAll();
+
+        bool Save();
+
     }
 }
