8c7083d66ac06ebc2a16d77b2c4c55af3f6cccb6
Sandun Prasanga <Sandun Prasanga@SANDUNPRASANGA>
2017-07-11 13:42:25 +0530

    branchResourceAllocation Manager Implemented

--- OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.Service.Contracts/IBranchResourceAllocationManager.cs
+++ OpenQbit.InstituteSystem.git/OpenQbit.Institute.BLL.Service.Contracts/IBranchResourceAllocationManager.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
+using OpenQbit.Institute.Common.Models;
 
 namespace OpenQbit.Institute.BLL.Service1.Contracts
 {
     public interface IBranchResourceAllocationManager
     {
+
+        bool Create(BranchResourceAllocation branchResourceAllocation);
+        BranchResourceAllocation FindById(int branchResourceAllocationId);
+        bool DeleteById(int branchResourceAllocationId);
+
+        bool Delete(BranchResourceAllocation branchResourceAllocation);
+
+        BranchResourceAllocation Find(Expression<Func<BranchResourceAllocation, bool>> predicate);
+
+        List<BranchResourceAllocation> FindList(Expression<Func<BranchResourceAllocation, bool>> predicate);
+
+        bool Update(BranchResourceAllocation branchResourceAllocation);
+
+        List<BranchResourceAllocation> GetAll();
+
+        bool Save();
 
     }
 }
